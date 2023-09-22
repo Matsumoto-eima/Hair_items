4f350370b4800c8ed04417e6da3f7725599a064d
Matsumoto-eima <135197614+Matsumoto-eima@users.noreply.github.com>
2023-09-22 06:54:45 +0000

    add_9

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,39 @@
 class Public::UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit,:update]
+
   def show
+    @user = User.find(params[:id])
+    @items = @user.items
+    @item = Item.new
   end
 
   def edit
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to current_user
+  end
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   def check
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
