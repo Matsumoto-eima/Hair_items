33865ce3cb8dde9c12e78be74fdd43ac80de0bcd
Matsumoto-eima <135197614+Matsumoto-eima@users.noreply.github.com>
2023-10-25 07:57:48 +0000

    add_21

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application";
+
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
   let raty =  new Raty(elem,opt)
